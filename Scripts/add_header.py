b8466c15f0fc2c9e627825920eb660948d24abbe
johnnymac647 <jmcnamara7@gatech.edu>
2021-12-21 01:08:19 -0500

    spines, headers, and removed X's good in the original trilogy

--- Scripts/add_header.py
+++ Scripts/add_header.py
@@ -2,7 +2,8 @@ import os
 import pandas as pd
 import sys
 
-def insert(originalfile,string):
+def insert(originalfile,string, dir):
+    originalfile = dir+originalfile
     with open(originalfile,'r') as f:
         with open('newfile.txt','w') as f2: 
             f2.write(string)
@@ -10,7 +11,7 @@ def insert(originalfile,string):
     os.remove(originalfile)
     os.rename('newfile.txt',originalfile)
 
-def add_header(file_name, sheet):
+def add_header(file_name, sheet, dir):
     # Header variables that will remain constant for all pieces in the corpus
     com = "!!!COM: John Williams"
     enc = "!!!ENC: Frank Lehman"
@@ -34,28 +35,31 @@ def add_header(file_name, sheet):
     df = df_list[int(sheet)]
 
     num = file_name.split('/')[-1].split("_")[0]
-    row = df[df["Number"] == num]
-    film_year = row["Film/Year Introduced"].iloc[0].split('-')
-    film = "!!!GAW: Star Wars: "+film_year[0].strip()
-    year = "!!!ODT: "+film_year[1].strip()
-    title = "!!!OTL: "+row["Theme"].iloc[0]
-    #title = "!!!OTL: " + " ".join(file_name.split("_")[1:][:-1])
+    if num != ".DS":
+        row = df[df["Number"] == num]
+        film_year = row["Film/Year Introduced"].iloc[0].split('-')
+        film = "!!!GAW: Star Wars: "+film_year[0].strip()
+        year = "!!!ODT: "+film_year[1].strip()
+        title = "!!!OTL: "+row["Theme"].iloc[0]
+        #title = "!!!OTL: " + " ".join(file_name.split("_")[1:][:-1])
 
-    header_components = [com, title, film, year, enc, eed1, eed2]
-    header = '\n'.join(header_components)+'\n'
+        header_components = [com, title, film, year, enc, eed1, eed2]
+        header = '\n'.join(header_components)+'\n'
 
-    insert(file_name, header)
+        insert(file_name, header, dir)
 
-    print(header)
+        print(header)
 
-    return
+        return
 
 
 def main(argv):
     if len(argv) > 2:
-        file_name = argv[1]
+        dir = argv[1]
         sheet = argv[2]
-        add_header(file_name, sheet)
+        files = os.listdir(dir)
+        for f in files:
+            add_header(f, sheet, dir)
         return
     else:
         print("Not enough arguments passed")
--- Scripts/final_rn2harm.py
+++ Scripts/final_rn2harm.py
@@ -6,47 +6,48 @@ def rn2harm(filename):
     split = os.path.splitext(filename)[0]
     file = open(filename, 'r')
     new_line = ""
-    for line in file:
-        if re.search('^\S+\t(([viVI]+)[^.]+)\t.+\t([^. ]+)$', line):
-            new_search = re.search('^\S+\t(([viVI]+)[^.]+)\t.+\t([^. ]+)$', line)
-            harte = new_search.groups()[2]
-            degrees = re.findall('\([^)]+\)', harte)
-            harte_search = re.findall('[b#*]*\d+', degrees[0])
-            # re.search('\d+]', new_search.groups()[2])
-            # print(re.search('[b#]*\d+]', new_search.groups()[2]))
-            for i, deg in enumerate(harte_search):
-                num = re.search('\d+', deg)
-                acc = re.search('[b#]+', deg)
-                non_perf = {  
-                    "bb": "D",
-                    "b": "m",
-                    "#": "A",
-                    "##": "AA"
-                }
-                perf = {
-                    "bb": "DD",
-                    "b": "D",
-                    "#": "A",
-                    "##": "AA"
-                }
-                harm_deg = ""
-                if num.group() == '2' or num.group() == '3' or num.group() == '6' or num.group() == '7' or num.group() == '9' or num.group() == '10' or num.group() == '13':
-                    if acc != None:
-                        harm_deg = non_perf[acc.group()]+num.group()
-                    else:
-                        harm_deg = "M"+num.group()
-                elif num.group() == '4' or num.group() == '5' or num.group() == '11' or num.group == '12':
-                    if acc != None:
-                        harm_deg = perf[acc.group()]+num.group()
-                    else:
-                        harm_deg = "P"+num.group()
-                harte_search[i] = harm_deg
-            harm = new_search.groups()[1]+''.join(harte_search)
-            new_line = line.replace(new_search.groups()[0], harm)
-        else:
-            new_line = line
-        newfile = open(split+'_rn2harm'+'.krn', 'a')
-        newfile.write(new_line)
+    if split != ".DS_Store":
+        for line in file:
+            if re.search('^\S+\t(([viVI]+)[^.]+)\t.+\t([^. ]+)$', line):
+                new_search = re.search('^\S+\t(([viVI]+)[^.]+)\t.+\t([^. ]+)$', line)
+                harte = new_search.groups()[2]
+                degrees = re.findall('\([^)]+\)', harte)
+                harte_search = re.findall('[b#*]*\d+', degrees[0])
+                # re.search('\d+]', new_search.groups()[2])
+                # print(re.search('[b#]*\d+]', new_search.groups()[2]))
+                for i, deg in enumerate(harte_search):
+                    num = re.search('\d+', deg)
+                    acc = re.search('[b#]+', deg)
+                    non_perf = {  
+                        "bb": "D",
+                        "b": "m",
+                        "#": "A",
+                        "##": "AA"
+                    }
+                    perf = {
+                        "bb": "DD",
+                        "b": "D",
+                        "#": "A",
+                        "##": "AA"
+                    }
+                    harm_deg = ""
+                    if num.group() == '2' or num.group() == '3' or num.group() == '6' or num.group() == '7' or num.group() == '9' or num.group() == '10' or num.group() == '13':
+                        if acc != None:
+                            harm_deg = non_perf[acc.group()]+num.group()
+                        else:
+                            harm_deg = "M"+num.group()
+                    elif num.group() == '4' or num.group() == '5' or num.group() == '11' or num.group == '12':
+                        if acc != None:
+                            harm_deg = perf[acc.group()]+num.group()
+                        else:
+                            harm_deg = "P"+num.group()
+                    harte_search[i] = harm_deg
+                harm = new_search.groups()[1]+''.join(harte_search)
+                new_line = line.replace(new_search.groups()[0], harm)
+            else:
+                new_line = line
+            newfile = open(split+'_rn2harm'+'.krn', 'a')
+            newfile.write(new_line)
 
 def main():
     for f in os.listdir():
