15db41218e54fbe94edcf92b6661999c1eb0cbc3
johnnymac647 <jmcnamara7@gatech.edu>
2022-02-10 20:44:40 -0500

    Update remove_empty.sh

--- Scripts/remove_empty.sh
+++ Scripts/remove_empty.sh
@@ -5,11 +5,20 @@ for i in "$@"
 do
     old_name="$i"
     new_name=${old_name%.*}_remove_empty.krn
-    for j in `seq 1 7`;
+    array=$(seq 1 7)
+    k=0
+    for j in $array;
     do
-        spine= extract -f $j $i; 
-        # removed= $spine rid -GLId;
-        # expr= grep '^[^=]' $removed;
-        echo $spine
+        spine= extract -f $j $i;
+        removed= extract -f $j $i | ridx -GLIdM;
+        echo "$removed"
+        echo "________"
+    #     # if [ -z "$removed" ]
+    #     # then
+    #     #     echo "print"
+    #     #     # echo $removed;
+    #     #     # assemble new_name spine > new_name;
+    #     # fi
     done
+    # # echo "$removed"
 done
