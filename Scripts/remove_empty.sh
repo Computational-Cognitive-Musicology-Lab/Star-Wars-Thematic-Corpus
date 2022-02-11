b9559affeccc9eb739843b4f3c31ba6ef90b6959
johnnymac647 <jmcnamara7@gatech.edu>
2022-02-10 21:13:23 -0500

    can python save us, no, I do not think it can

--- Scripts/remove_empty.sh
+++ Scripts/remove_empty.sh
@@ -6,19 +6,24 @@ do
     old_name="$i"
     new_name=${old_name%.*}_remove_empty.krn
     array=$(seq 1 7)
-    k=0
+    let k=0
     for j in $array;
     do
-        spine= extract -f $j $i;
+        # spine= extract -f $j $i;
+        let k++;
         removed= extract -f $j $i | ridx -GLIdM;
-        echo "$removed"
-        echo "________"
-    #     # if [ -z "$removed" ]
-    #     # then
-    #     #     echo "print"
-    #     #     # echo $removed;
-    #     #     # assemble new_name spine > new_name;
-    #     # fi
+        # echo "$removed";
+        # myVar=`echo $removed | sed 's/ *$//g'`
+        # echo $myVar
+        python "../../Scripts/remove_empty.py" $k
+        # if [ "$myVar" == '' ];
+        # then
+        #     # echo $k
+        #     # echo "is empty"
+        #     # assemble new_name spine > new_name;
+        # else
+        #     # echo "not empty"
+        # fi
     done
-    # # echo "$removed"
+
 done
