6614b0dbe7140e304bff9effd0641a09634f4626
johnnymac647 <jmcnamara7@gatech.edu>
2022-03-18 10:31:07 -0400

    modified fix_intervals script. Committing before testing

--- Scripts/fix_intervals.sh
+++ Scripts/fix_intervals.sh
@@ -1,39 +1,35 @@
 #!/bin/sh
+###############################################################################################
+# Test for converting RNtxt to **harm
+# Note: Using Frank Lehman's analyses encoded in RNtxt as sample material.
 
+# Parser test1
+
+# NOTES
+# inversions convert from numeric to alphabet;
+# augmented and diminished triads are already the same;
+# for some reason M is a designation for M7 but not m for m7??
+# flat intervals are converted to minor if they are dissonant or imperfect; dim if they are perfect;
+# RNtxt allows inversions to be written with OR without slash so keep this line for caution (it doesn't hurt)
+# because of om7 and oD7 for all inversions all types of dim7 must be dealt with twice;
+# \\ only indicates phrase ends and not beginnings, so translation technically makes incorrect humdrum.
+# #viio is considered courtesy accidental so translating could have problems
+# humdrum does not have symbol for "no chord". We adopt "NC" since "Nc" would mean Neapolitan in 2nd inv.
+# Rntxt indicates a beat with nothing following the beat (so two beats in a row) to indicate NC/@none
+# xxx this currently uses sed not humsed so it will translate ALL text (note Ger and It below);
+# NOTE: NOT FIXED FOR DOUBLE FLATS
+
+###############################################################################################
 for i in "$@"
 do
-old_name="$i"
-new_name=${old_name%.*}_fixedFlat.krn
-sed -E -e 's/([ABCDEFG])b/\1-/g' ${i} > $new_name
-done
-
-exit
-
-
-for i in "$@"
-do
-
 old_name="$i"
 new_name=${old_name%.*}_fixedInterval.krn
-
-    
-    sed -E -e 's/m2/b2/g' \
-    -Ee 's/([^*M])M2/\12/g' \
-    -Ee 's/A2/#2/g'
-    -Ee 's/(^[^?])*m3([^?]*$)/\1b3\2/g' \
-	-Ee 's/(^[^?])*M3([^?]*$)/\13\2/g' ${i} > $new_name  
-    # -Ee 's/A4/#4/g' \
-    # -Ee 's/D5/b5/g' \
-    # -Ee 's/D6/bb6/g' \
-    # -Ee 's/m6/b6/g' \
-    # -Ee 's/M6/6/g' \
-    # -Ee 's/A6/#6/g' \
-    # -Ee 's/D7/bb7/g' \
-    # -Ee 's/m7/b7/g' \
-    # -Ee 's/M7/7/g' \
-    # -Ee 's/m9/b9/g' \ 
-    # -Ee 's/M9/9/g' ${i} > $new_name 
-    
-
+sed -E -e 's/(^[^?]*)m([23679].*$)/\1b\2/g' \
+    -Ee 's/(^[^?*]*)M([23679].*$)/\1\2/g' \
+    -Ee 's/(^[^?]*)D([23679].*$)/\1bb\2/g' \
+    -Ee 's/(^[^?]*)A([23679].*$)/\1#\2/g' \
+    -Ee 's/(^[^?]*)D([458].*$)/\1b\2/g' \
+    -Ee 's/(^[^?]*)A([458].*$)/\1#\2/g' ${i} > $new_name
 done
+
 exit
